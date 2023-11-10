93f2e7410fab6a1a1281e4862a84ec536def8a46
SSPlusUltra <52967609+SSPlusUltra@users.noreply.github.com>
2023-11-10 00:11:37 -0500

    router resolved

--- server.js
+++ server.js
@@ -1,51 +1,46 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const router = require('./routes/router')
-const mongoose = require('mongoose')
-require('dotenv/config')
+const express = require('express');
+const cors = require('cors');
+const router = require('./routes/router');
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv/config');
 
-const dbOptions = {useNewUrlParser:true, useUnifiedTopology: true}
-const port=process.env.PORT|| 4000
 
-const connectDB = async ()=>{
-    try{
-        const conn = await mongoose.connect(process.env.DB_URI, dbOptions);
-        console.log(`MOngoDB connected:${conn.connection.host}`)
 
-    }
-    catch(error){
-        console.log(error);
-        process.exit(1);
-    }
-}
+const port =4000;
 
 const app = express();
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
 
-const corsOptions = {
-    origin: '*',
-    credentials: true,
-    optionSuccessStatus:200
-} 
+const dirname = path.resolve();
 
-app.use(cors(corsOptions))
-app.use('/', router)
-
-app.use(express.static(path.join(__dirname, "./client/build")));
-
-app.get("*", function(_, res){
-    res.sendFile(
-        path.join(__dirname, "./client/build/index.html"),
-        function(err){
-            res.status(500).send(err)
-        }
-    )
+app.use(cors());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json({extended:true}));
+app.use('/',router)
+app.use(express.static(path.join(dirname, "client/build")))
+app.get('*', function(_, res){
+    res.sendFile(path.join(dirname, "client/build/index.html"), function(err){
+        res.status(500).send(err);
+    })
 })
 
-connectDB().then(()=>{
-    app.listen(port, ()=>{
-        console.log(`server is running on ${port}`)
-    }) 
+const dbOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(process.env.DB_URI, dbOptions);
+      console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  };
+
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+  });
 });
